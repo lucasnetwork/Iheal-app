570ad76c4e30c37dd7b800c41d3077c5f4c8152c
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-07-29 15:50:03 -0300

    add context to cart values and function to modify yout values

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import Tabs from './routes/Tabs';
+import ContextProvider from './services/context';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function Main() {
   return (
-    <>
+    <ContextProvider>
       <NavigationContainer>
         <Tabs />
       </NavigationContainer>
       <StatusBar style="light" backgroundColor="#11BAFD" />
-    </>
+    </ContextProvider>
   );
 }
---
+++ src/services/context.tsx
@@ -0,0 +1,143 @@
+import React, { createContext, useState } from 'react';
+
+type product = {
+  id: number;
+  name: string;
+  price: number;
+  quantity?: number;
+  total?: number;
+  priceFormat?: string;
+  priceFormatTotal?: string;
+};
+
+type cartProps = {
+  products: Array<product>;
+  totalQuantity: number;
+  total: number;
+  totalPriceFormat: string;
+};
+
+interface constexProps {
+  cart: cartProps;
+  addItem(newProduct: product, quantity: number): void;
+  DeleteItem(id: number, quantity: number): void;
+}
+
+export const ContextApp = createContext({} as constexProps);
+
+const ContextProvider: React.FC = ({ children }) => {
+  const [cart, setCart] = useState<cartProps>({
+    products: [],
+    totalQuantity: 0,
+    total: 0,
+    totalPriceFormat: 'R$ 0,00',
+  });
+
+  function updateItem(newProduct: product, quantity: number) {
+    const newCart = { ...cart };
+    const newProducts = newCart.products.map(productSingle => {
+      if (productSingle.id === newProduct.id) {
+        return {
+          ...productSingle,
+          total: (productSingle.total || 0) + productSingle.price * quantity,
+          quantity: (productSingle.quantity || 0) + quantity,
+          priceFormatTotal: `R$ ${(
+            (productSingle.total || 0) +
+            productSingle.price * quantity
+          )
+            .toFixed(2)
+            .replace('.', ',')}`,
+        };
+      }
+      return productSingle;
+    });
+    newCart.products = newProducts;
+    newCart.total += newProduct.price * quantity;
+    newCart.totalPriceFormat = `R$ ${newCart.total
+      .toFixed(2)
+      .replace('.', ',')}`;
+
+    setCart(newCart);
+  }
+
+  function addNewItem(newProduct: product, quantity: number) {
+    const newCart = { ...cart };
+    newCart.products = [
+      ...newCart.products,
+      {
+        ...newProduct,
+        total: newProduct.price * quantity,
+        quantity,
+        priceFormatTotal: `R$ ${(newProduct.price * quantity)
+          .toFixed(2)
+          .replace('.', ',')}`,
+        priceFormat: `R$ ${newProduct.price.toFixed(2).replace('.', ',')}`,
+      },
+    ];
+    newCart.total += newProduct.price * quantity;
+    newCart.totalPriceFormat = `R$ ${newCart.total
+      .toFixed(2)
+      .replace('.', ',')}`;
+    setCart(newCart);
+  }
+
+  function addItem(newProduct: product, quantity: number) {
+    const existProduct = cart.products.find(
+      productSingle => productSingle.id === newProduct.id
+    );
+
+    if (existProduct) {
+      updateItem(newProduct, quantity);
+      return;
+    }
+    addNewItem(newProduct, quantity);
+  }
+
+  function DeleteItem(id: number, quantity: number) {
+    const newCart = { ...cart };
+    const existProduct = cart.products.find(
+      productSingle => productSingle.id === id
+    );
+    if (!existProduct || (existProduct.quantity || 0) < quantity) {
+      return;
+    }
+
+    newCart.totalQuantity -= quantity;
+    newCart.total -= quantity * (existProduct.price || 0);
+    newCart.totalPriceFormat = `R$ ${newCart.total
+      .toFixed(2)
+      .replace('.', ',')}`;
+
+    if (existProduct?.quantity === quantity) {
+      newCart.products = newCart.products.filter(
+        singleProduct => singleProduct.id !== id
+      );
+
+      setCart(newCart);
+      return;
+    }
+    newCart.products = newCart.products.map(singleProduct => {
+      if (singleProduct.id === id) {
+        const newProduct = { ...singleProduct };
+
+        newProduct.quantity = (newProduct.quantity || 0) - quantity;
+        newProduct.total =
+          (newProduct.total || 0) - singleProduct.price * quantity;
+        newProduct.priceFormatTotal = `R$ ${(newProduct.total * quantity)
+          .toFixed(2)
+          .replace('.', ',')}`;
+        return newProduct;
+      }
+      return singleProduct;
+    });
+    setCart(newCart);
+  }
+
+  return (
+    <ContextApp.Provider value={{ cart, addItem, DeleteItem }}>
+      {children}
+    </ContextApp.Provider>
+  );
+};
+
+export default ContextProvider;
