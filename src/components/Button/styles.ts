573b89b68a67970a788a54c27207357ad717355c
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-08-01 11:20:45 -0300

    add button Component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    width: '100%',
+    borderRadius: 8,
+    backgroundColor: '#FDC500',
+    height: 40,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#01579B',
+    fontSize: 12,
+    textAlign: 'center',
+  },
+  small: {
+    borderRadius: 100,
+  },
+});
