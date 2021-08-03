40c56bf50bc72782a8b463003d27567cc0196368
julys martins <julysmartins@DESKTOP-E3AKR88.localdomain>
2021-08-03 19:50:34 -0300

    change mapProducts

--- src/api/map-data-products.ts
+++ src/api/map-data-products.ts
@@ -7,12 +7,28 @@ export type productData = {
   priceFormat: number;
   image: string;
   stock: number;
-  user_creator: any;
+  user_creator: {
+    _id: string;
+    username: string;
+    email: string;
+    address: string;
+  };
 };
 // eslint-disable-next-line import/prefer-default-export
 export const mapData = (pagesData = [{}] as any): productData[] =>
   pagesData.map((data: any): productData => {
     const { url } = data.image || { url: '' };
+    const {
+      id: _id,
+      username,
+      email,
+      address,
+    } = data.user_creator || {
+      id: '',
+      username: '',
+      email: '',
+      address: '',
+    };
     const {
       id,
       name,
@@ -26,7 +42,12 @@ export const mapData = (pagesData = [{}] as any): productData[] =>
       id,
       name,
       priceFormat,
-      user_creator,
+      user_creator: {
+        _id,
+        username,
+        email,
+        address,
+      },
       stock,
       image: url,
       description,
