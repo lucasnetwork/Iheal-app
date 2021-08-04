c2c7736877bfa86c7a57ce8f5b477fa23a75dcdd
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-08-04 15:06:46 -0300

    add notification component

---
+++ src/components/Notification/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    width: '72%',
+    minHeight: 40,
+    padding: 13,
+    backgroundColor: 'rgba(253,197,0,.38)',
+    position: 'absolute',
+    bottom: 90,
+    left: 50,
+  },
+
+  text: {
+    fontSize: 12,
+    color: '#01579B',
+  },
+});
