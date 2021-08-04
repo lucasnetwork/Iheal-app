e295369917f37874e5f48dd78ef414982731a82a
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-08-04 14:34:58 -0300

    add account client when logged

--- src/views/Account/styles.ts
+++ src/views/Account/styles.ts
@@ -24,4 +24,61 @@ export default StyleSheet.create({
   Containerbutton: {
     width: 123,
   },
+
+  orderContainer: {
+    marginBottom: 10,
+    borderColor: '#DADADA',
+    borderWidth: 1,
+    borderRadius: 8,
+    height: 107,
+    flexDirection: 'row',
+  },
+  containerTitle: {
+    marginTop: 25,
+    marginHorizontal: 38,
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.2)',
+    paddingBottom: 16,
+    marginBottom: 10,
+  },
+  appInfo: {
+    paddingTop: 17,
+    borderTopColor: 'rgba(0, 0, 0, 0.2)',
+    borderTopWidth: 1,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+  },
+  loggoutText: {
+    color: '#A8A8B3',
+    fontSize: 13,
+    marginBottom: 16,
+  },
+  appInfoText: {
+    fontSize: 10,
+    color: '#A8A8B3',
+    fontWeight: '500',
+  },
+  loggoutContainer: {
+    marginHorizontal: 78,
+    paddingTop: 24,
+    paddingBottom: 32,
+    alignItems: 'center',
+  },
+  titleShop: {
+    color: '#00509D',
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  email: {
+    color: '#A8A8B3',
+    fontSize: 14,
+    fontWeight: '500',
+  },
+
+  historyTitle: {
+    fontSize: 13,
+    fontWeight: '500',
+    color: '#A8A8B3',
+    marginLeft: 39,
+  },
 });
