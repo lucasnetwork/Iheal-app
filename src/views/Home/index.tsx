8cd34b6e9be05d5956f1d01f1ee3700e4d4f8caa
Kelvyn Reis <kelvyn2reis@gmail.com>
2021-08-10 18:48:45 -0300

    error 404

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,11 +1,10 @@
-import { styles } from './styles';
 import React from 'react';
 import { View } from 'react-native';
 
 export default function Home() {
   return (
     <>
-      <View style={styles.container} />
+      <View />
     </>
   );
 }
